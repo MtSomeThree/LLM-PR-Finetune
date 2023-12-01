cf8e73104684ca0515e6730fa07c1828df5c4325
MtSomeThree <tmeng@cs.ucla.edu>
2023-12-01 02:31:17 -0800

    upd

--- toxiGen.py
+++ toxiGen.py
@@ -3,20 +3,32 @@ import json
 from transformers import pipeline
 from transformers import LlamaForCausalLM, LlamaTokenizer, LlamaModel
 from datasets import load_dataset
+from undecorated import undecorated
+from types import MethodType
+from torch.optim import AdamW, Adam
 import torch
 import time
+import math
 
 def oracle(texts, pipeline):
 	outputs = pipeline(texts)
-	score = 0.0
+	scores = []
 	for output in outputs:
 		if output['label'] == 'LABEL_1':
-			score += 1.0
-	return score
+			scores.append(1.0)
+		else:
+			scores.append(0.0)
+	return scores
 
 def reinforcement_train(model, tokenizer, pipeline, train_data, test_data, args):
 	cnt = 0
 	total_score = 0.0
+
+	generate_with_grad = undecorated(model.generate)
+	model.generate_with_grad = MethodType(generate_with_grad, model)
+
+	optimizer = AdamW(params=model.parameters(), lr=args.lr)
+
 	for data in test_data:
 		prompt = data['text']
 		prompts = [prompt] * args.batch_size
@@ -24,24 +36,44 @@ def reinforcement_train(model, tokenizer, pipeline, train_data, test_data, args)
 		input_ids = torch.tensor(encodings_dict['input_ids']).to(args.device)
 		attention_mask = torch.tensor(encodings_dict['attention_mask']).to(args.device)
 		prompt_length = len(input_ids[0])
-		outputs = model.generate(
+		outputs = model.generate_with_grad(
 				input_ids=input_ids,
 				attention_mask=attention_mask,
 				do_sample=True,
-				temperature=0.1,
-				top_p=0.9,
 				max_new_tokens=args.max_length,  # desired output sentence length
 				pad_token_id=model.config.eos_token_id,
 				output_scores=True,
 				return_dict_in_generate=True,
 			)
+
+		pos = 0
+		log_probs = None
+		for scores in outputs.scores:
+			log_prob = torch.log_softmax(scores, dim=1)
+			token_log_prob = torch.gather(log_prob, dim=1, index=outputs.sequences[:, prompt_length + pos].unsqueeze(1)).squeeze()
+			if log_probs is None:
+				log_probs = token_log_prob
+			else:
+				log_probs = log_probs + token_log_prob
+			pos += 1
+
+		if math.isinf(log_probs.sum().item()):
+			print ("INF!")
+
 		gen_text = tokenizer.batch_decode(outputs['sequences'][:, prompt_length:])
-		score = oracle(gen_text, pipeline)
-		total_score += score
+		rewards = oracle(gen_text, pipeline)
+		rewards = torch.Tensor(rewards)
+		loss = log_probs.dot(rewards)
+
+		print (log_probs, rewards)
+		
+		optimizer.zero_grad()
+		loss.backward()
+		optimizer.step()
+
 		cnt += 1
 		if cnt >= args.size:
 			break
-	print (total_score / float(cnt) / float(args.batch_size))
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
@@ -51,7 +83,8 @@ if __name__ == "__main__":
 	parser.add_argument('--device', type=str, default=None)
 	parser.add_argument('--oracle_device', type=str, default=None)
 	parser.add_argument('--max_length', type=int, default=32)
-	parser.add_argument('--batch_size', type=int, default=1)
+	parser.add_argument('--batch_size', type=int, default=2)
+	parser.add_argument('--lr', type=float, default=3e-5)
 	args = parser.parse_args()
 
 	if args.cuda == -1:
